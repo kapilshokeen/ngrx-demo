0a0674b4f272771fb5c56a7b70d3e47ffafeec3c
Kapil Shokeen <kapilshokeen@ymail.com>
2025-07-26 10:03:33 +0530

    added store features in post-list component

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { postListClear, postListFetch } from '../../store/post-store/post.actions';
+import { postListSelector } from '../../store/post-store/post.selectors';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
-  standalone: false,
+  standalone: true,
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.scss'
+  styleUrl: './post-list.component.scss',
+  imports: [
+    AsyncPipe,
+    RouterLink
+  ]
 })
 export class PostListComponent {
+
+  private store = inject(Store);
+
+  protected postList$ = this.store.select(postListSelector);
+
+  ngOnInit() {
+    this.store.dispatch(postListFetch());
+  }
+
+  ngOnDestroy() {
+    this.store.dispatch(postListClear());
+  }
 
 }
